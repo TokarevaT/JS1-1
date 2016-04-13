a6d025a7e4fb10d87dac7c93685251f263997cd1
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-04-13 04:09:35 +0300

    math.pow

---
+++ js11/js/JavaScript1.js
@@ -0,0 +1,27 @@
+﻿function pow(x, n) {
+    var result = x;
+    for (var i = 1; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}
+var x = prompt("Введите число", "");
+console.log('x', x);
+var n = prompt("Введите степень", "");
+console.log('n', n)
+
+if (n <= 1) {
+    alert('Степень ' + n +
+      'не поддерживается, введите целую степень, большую 1'
+    );
+} else {
+    alert(pow(x, n));
+    console.log('(pow(x, n))', (pow(x, n)));
+}
+//var x = +prompt("Введите первое число", "");
+//console.log('x',x);
+//var n = +prompt("Введите второе число", "");
+//console.log('n', n);
+
+//alert(x + n);
+//console.log('x + n', x + n);
